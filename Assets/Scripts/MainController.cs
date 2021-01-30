ef4c353f217c88e6f8a42daef4d5c37fc989df7d
haskicbr <haskicbr@gmail.com>
2021-01-30 18:03:14 +0300

    add assets

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MainController : MonoBehaviour
+{
+
+    public GameObject particle;
+    public GameObject gameWorld;
+    public LayerMask clickMask;
+    public NavMeshAgent playerAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            Vector3 clickPosition;
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000f, clickMask))
+            {
+                clickPosition = hit.point;
+                //Instantiate(particle, new Vector3(clickPosition.x, 10.0f, clickPosition.z), particle.transform.rotation);
+
+                //Vector3 direction = particle.transform.position -  new Vector3(clickPosition.x, 10.0f, clickPosition.z);
+                //particle.GetComponent<Rigidbody>().AddForceAtPosition(direction.normalized, particle.transform.position);
+                //particle.transform.position = new Vector3(clickPosition.x, 1.0f, clickPosition.z);
+
+
+                playerAgent.SetDestination(clickPosition);
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Transform _rotator;
+
+    private float currentHealth = 100f;
+    private float currentStamina = 100f;
+
+    private float maxStamina = 100f;
+    private float maxHealth = 100f;
+
+
+    public Image healthBar;
+    public Image staminaBar;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 0.5f;
+/*        float test = this.staminaBar.rectTransform.sizeDelta.y;
+        this.staminaBar.rectTransform.sizeDelta = new Vector2(100f, test);
+        this.staminaBar.rectTransform.right = new Vector2(100f, test);*/
+
+        _rotator = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {/*
+        _rotator.position = new Vector3(_rotator.position.x + 10f * Time.deltaTime, _rotator.position.y, _rotator.position.z);
+        _rotator.Rotate(0, speed * Time.deltaTime, 0);*/
+    }
+}
